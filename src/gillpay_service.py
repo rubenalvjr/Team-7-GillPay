cb5ea10c842d31dd22dd59571665cb38b4d7bad3
Ruben Alvarez <rubenalvjr@gmail.com>
2025-09-04 00:03:04 -0500

    Refactored code to match course coding standards and add GillPayService

---
+++ src/gillpay_service.py
@@ -0,0 +1,29 @@
+class GillPayService:
+
+    # TODO - Create logic to handle total income
+    def GetTotalIncome(self):
+        # Call TransactionDAO to get list of transactions
+        # Filter out only Income transactions
+        # Sum all the data
+        # Return data
+        return None
+
+    # TODO - Create logic to handle total expenses
+    def GetTotalExpenses(self):
+        # Call TransactionDAO to get list of transactions
+        # Filter out only Expense transactions
+        # Sum all the data
+        # Return data
+        return None
+
+    # TODO - Create logic to handle net income
+    def GetNetIncome(self):
+        # Call GetTotalIncome function to get total income
+        # Call GetTotalExpenses function to get total income
+        # Subtract total income data from total expense data
+        # Return data
+        return None
+
+    # TODO - Create logic to adding a new Transaction
+    def PostTransaction(self):
+        return None
--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from src.transaction_dao import TransactionDAO
 from src.transaction import Transaction
 
-transaction_dao = TransactionDAO()
-#transaction = Transaction("Expense","Entertainment", 20, "2025/08/15")
 
-#transaction_dao.save_transaction(transaction)
-print(transaction_dao.get_all_transactions())
+# TODO - No longer interact with DAO directly
+# TODO - Only interact with GillPayService
+
+# transaction_dao = TransactionDAO()
+# transaction = Transaction("Expense","Entertainment", 20, "2025/08/15")
+#
+# transaction_dao.save_transaction(transaction)
+# print(transaction_dao.get_all_transactions())
